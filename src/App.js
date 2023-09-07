99a542eebc4ff3eedd40333f7e24026a556d6cbb
Bao Nguyen <bnguyen88@knights.ucf.edu>
2023-09-07 11:16:46 -0400

    add code for app, index, body, nav, and footer js files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Body from "./component/Body";
+import Nav from "./component/Nav";
+import Footer from "./component/Footer";
+
+const App = () => {
+    return (
+        <>
+            <Nav />
+            <Body />
+            <Footer/>
+        </>
+    );
+}
+
+export default App;
---
+++ src/component/Body.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Nav from './Nav';
+import AboutMe from './Pages/AboutMe';
+import Portfolio from './Pages/Portfolio';
+import Contact from './Pages/Contact';
+import Resume from './Pages/Resume';
+
+export default function Body() {
+  const [currentPage, setCurrentPage] = useState('AboutMe');
+
+  const renderPage = () => {
+    if (currentPage === 'AboutMe') {
+      return <AboutMe />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
